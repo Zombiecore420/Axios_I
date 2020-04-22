d27af31e9d4c359b773a367e0020d7e73f9e61b5
Alex Nail Quiroz Palma <al221810641@gmail.com>
2020-04-21 19:10:36 -0500

    Creación de clase App

--- src/App.js
+++ src/App.js
@@ -3,3 +3,24 @@ import axios from 'axios';
 import './App.css';
 import App from './App';
 
+class App extends Component{
+  constructor(){
+  super();
+  
+  this.state = {
+    names:[]
+  };
+  
+  axios.get("http://localhost:3001/names")
+  .then(response =>{
+    this.setState({
+      names: response.data
+  })
+  })
+  .catch(error=> {
+    console.log(error);
+  })
+  }
+  
+ 
+  
